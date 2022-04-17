126c07a228583dd7f3743299304994e9b8a3bdce
Ilinca George-Cosmin <ilincageorgecosmin@yahoo.com>
2022-04-17 17:31:23 +0300

    Implementing GET Methods for User Resource

--- src/main/java/com/rest/webservices/restfulwebservices/user/UserDaoService.java
+++ src/main/java/com/rest/webservices/restfulwebservices/user/UserDaoService.java
@@ -32,7 +32,8 @@ public class UserDaoService {
     public User findOne(int id) {
         for (User user : users) {
             if (user.getId() == id)
-                return user
+                return user;
         }
+        return null;
     }
 }
