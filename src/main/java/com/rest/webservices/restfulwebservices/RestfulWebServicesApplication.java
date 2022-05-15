03a8169a1466c10d473924f71064285ef1605f95
Ilinca George-Cosmin <ilincageorgecosmin@yahoo.com>
2022-05-15 15:47:00 +0300

    Implement Dynamic Filtering for RESTful Service

--- src/main/java/com/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
+++ src/main/java/com/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
@@ -1,8 +1,11 @@
 package com.rest.webservices.restfulwebservices;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
@@ -22,5 +25,12 @@ public class RestfulWebServicesApplication {
         AcceptHeaderLocaleResolver localeResolver = new AcceptHeaderLocaleResolver();
         localeResolver.setDefaultLocale(Locale.US);
         return localeResolver;
+    }
+
+    @Configuration
+    public class JacksonConfiguration {
+        public JacksonConfiguration(ObjectMapper objectMapper) {
+            objectMapper.setFilterProvider(new SimpleFilterProvider().setFailOnUnknownId(false));
+        }
     }
 }
