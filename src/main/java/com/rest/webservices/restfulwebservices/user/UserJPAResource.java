e574f30aa1c38dbadc920cdfe50cb1a28918dab3
Ilinca George-Cosmin <ilincageorgecosmin@yahoo.com>
2022-06-02 14:38:07 +0300

    Updating POST and DELETE methods on User Resource

--- src/main/java/com/rest/webservices/restfulwebservices/user/UserJPAResource.java
+++ src/main/java/com/rest/webservices/restfulwebservices/user/UserJPAResource.java
@@ -39,7 +39,7 @@ public class UserJPAResource {
 
     @PostMapping("/jpa/users")
     public ResponseEntity<Object> createUser(@Valid @RequestBody User user) {
-        User savedUser = service.save(user);
+        User savedUser = userRepository.save(user);
         //CREATED
         // /user/{id}
         URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedUser.getId()).toUri();
@@ -49,8 +49,6 @@ public class UserJPAResource {
 
     @DeleteMapping("/jpa/users/{id}")
     public void deleteUser(@PathVariable int id) {
-        User user = service.deleteById(id);
-        if(user==null)
-            throw new UserNotFoundException("id-" + id);
+        userRepository.deleteById(id);
     }
 }
