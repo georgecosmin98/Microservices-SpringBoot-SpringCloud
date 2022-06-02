e7690ec70ec8396145e7c1257e0d57341bf132f4
Ilinca George-Cosmin <ilincageorgecosmin@yahoo.com>
2022-06-02 14:49:40 +0300

    Creating Post Entity and Many to One Relationship with User Entity

--- src/main/java/com/rest/webservices/restfulwebservices/user/User.java
+++ src/main/java/com/rest/webservices/restfulwebservices/user/User.java
@@ -6,9 +6,11 @@ import io.swagger.annotations.ApiModelProperty;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
+import java.util.List;
 
 @ApiModel(description = "All details about the user")
 @Entity
@@ -18,13 +20,16 @@ public class User {
     @GeneratedValue
     private Integer id;
 
-    @Size(min=2, message = "Name should have at least 2 characters")
+    @Size(min = 2, message = "Name should have at least 2 characters")
     @ApiModelProperty(notes = "Name should have at least 2 characters")
     private String name;
 
     @Past()
     @ApiModelProperty(notes = "Birth date should be in the past")
     private Date birthDate;
+
+    @OneToMany(mappedBy = "user")
+    private List<Post> posts;
 
     public User() {
     }
@@ -57,6 +62,14 @@ public class User {
 
     public void setBirthDate(Date birthDate) {
         this.birthDate = birthDate;
+    }
+
+    public List<Post> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(List<Post> posts) {
+        this.posts = posts;
     }
 
     @Override
